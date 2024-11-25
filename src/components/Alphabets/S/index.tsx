74bc4790d987136b87a863e5da82d7a2a3a2438a
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-11-25 22:19:22 +0900

    feat: S 컴포넌트 생성

--- src/components/Alphabets/S/index.tsx
+++ src/components/Alphabets/S/index.tsx
@@ -1,3 +1,7 @@
+import { randomColor } from "@/utils/randomColor";
+
+const RADIUS = 40;
+
 const drawS = ({
   x,
   y,
@@ -8,7 +12,23 @@ const drawS = ({
   ctx: CanvasRenderingContext2D;
 }) => {
   ctx.beginPath();
-  ctx.arc(x, y, 40, 0, Math.PI * 2);
+  ctx.arc(x, y, RADIUS, (1 / 2) * Math.PI, (3 / 2) * Math.PI);
+  ctx.fillStyle = randomColor();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.arc(x, y + RADIUS * 2, RADIUS, (3 / 2) * Math.PI, (1 / 2) * Math.PI);
+  ctx.fillStyle = randomColor();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.rect(x, y - RADIUS, RADIUS, RADIUS);
+  ctx.fillStyle = randomColor();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.rect(x - RADIUS, y + RADIUS * 2, RADIUS, RADIUS);
+  ctx.fillStyle = randomColor();
   ctx.fill();
 };
 
